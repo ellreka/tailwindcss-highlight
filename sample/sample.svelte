12fb4ebd328c4685cc534b930df0f63d6ca25239
ellreka <ellreka5364@gmail.com>
2022-07-03 21:58:25 +0900

    chore: add sample project

---
+++ sample/sample.svelte
@@ -0,0 +1,76 @@
+<div
+  class="grid grid-cols-1 sm:grid-cols-2 sm:gap-x-8 sm:px-8 sm:py-12 md:py-16"
+  transform="">
+  <div class="relative z-10 col-start-1 row-start-1 bg-gradient-to-t from-black px-4 pt-40 pb-3 sm:bg-none">
+    <p class="text-sm font-medium text-white sm:mb-1 sm:text-gray-500">
+      Entire house
+    </p>
+    <h2 class="text-xl font-semibold text-white sm:text-2xl sm:leading-7 sm:text-black md:text-3xl">
+      Beach House in Collingwood
+    </h2>
+  </div>
+  <div class="col-start-1 row-start-2 px-4 sm:pb-16">
+    <div class="my-5 flex items-center text-sm font-medium sm:mt-2 sm:mb-4">
+      <svg
+        width="20"
+        height="20"
+        fill="currentColor"
+        class="text-violet-600">
+        <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
+      </svg>
+      <div class="ml-1">
+        <span class="text-xl text-black">4.94</span>
+        <span class="sm:hidden md:inline">(128)</span>
+      </div>
+      <div class="mx-2 text-base font-normal">·</div>
+      <div>Collingwood, Ontario</div>
+    </div>
+    <hr class="hidden w-16 border-gray-300 sm:block" />
+  </div>
+  <div class="col-start-1 row-start-3 space-y-3 px-4">
+    <p class="flex items-center text-sm font-medium text-black">
+      <img
+        src="/kevin-francis.jpg"
+        alt=""
+        class="mr-2 h-6 w-6 rounded-full bg-gray-100"
+      />
+      Hosted by Kevin Francis
+    </p>
+    <button
+      type="button"
+      class="rounded-lg bg-violet-100 px-6 py-2 text-base font-semibold text-violet-700">
+      Check availability
+    </button>
+  </div>
+  <div class="col-start-1 row-start-1 flex sm:col-start-2 sm:row-span-3">
+    <div class="grid w-full grid-cols-3 grid-rows-2 gap-2">
+      <div class="relative col-span-3 row-span-2 md:col-span-2">
+        <img
+          src="/beach-house.jpg"
+          alt=""
+          class="absolute inset-0 h-full w-full bg-gray-100 object-cover sm:rounded-lg"
+        />
+      </div>
+      <div class="relative hidden md:block">
+        <img
+          src="/beach-house-interior.jpg"
+          alt=""
+          class="absolute inset-0 h-full w-full rounded-lg bg-gray-100 object-cover"
+        />
+      </div>
+      <div class="relative hidden md:block">
+        <img
+          src="/beach-house-view.jpg"
+          alt=""
+          class="absolute inset-0 h-full w-full rounded-lg bg-gray-100 object-cover"
+        />
+      </div>
+    </div>
+  </div>
+</div>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ sample/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mode: 'jit',
+  theme: {},
+  variants: {
+    extend: {}
+  }
+}
